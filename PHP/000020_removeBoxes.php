bff21dd42d5937078de4478da9571a7b17a33308
willianleiton <willianleitondev@gmail.com>
2025-02-23 19:31:15 -0500

    Dynamic programming - Hard level

--- PHP/000020_removeBoxes.php
+++ PHP/000020_removeBoxes.php
@@ -9,14 +9,16 @@ class Solution {
    * @param Integer[] $boxes
    * @return Integer
    */
-  function removeBoxes(array $boxes): int {
+  function removeBoxes(array $boxes): int
+  {
     $this->boxes = $boxes;
     $this->memo = [];
 
     return $this->calculateRange(0, count($boxes) - 1, 0);
   }
 
-  private function calculateRange(int $start, int $end, int $boxesCounter): int {
+  private function calculateRange(int $start, int $end, int $boxesCounter): int
+  {
     if ($start > $end) {
       return 0;
     }
